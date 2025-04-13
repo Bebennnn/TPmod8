bc694384a2be89e4bcb9d2c0f24f132202026d64
Benaya Giuseppe Linggaputra Sinulingga <Benaya.gls@gmail.com>
2025-04-13 21:29:52 +0700

    menyelesaikan program dengan runtime configuration

---
+++ tpmodul8_103022330122/Main.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul8_103022330122;
+
+class program
+{
+
+    static void Main(String[] args)
+    {
+        CovidConfig config = new CovidConfig();
+
+        config.UbahSatuan();
+
+        config = new CovidConfig();
+
+        Console.Write($"Berapa suhu badan anda saat ini? Dalam nilai {config.satuan_suhu}: ");
+        double suhu = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam? ");
+        int hari = Convert.ToInt32(Console.ReadLine());
+
+        bool suhuNormal = false;
+
+        Console.WriteLine($"Suhu kamu: {suhu} {config.satuan_suhu}");
+        Console.WriteLine($"Hari sejak demam: {hari}, batas: {config.batas_hari_deman}");
+
+
+        if (config.satuan_suhu == "celcius")
+            suhuNormal = suhu >= 36.5 && suhu <= 37.5;
+        else if (config.satuan_suhu == "fahrenheit")
+            suhuNormal = suhu >= 97.7 && suhu <= 99.5;
+
+        if (suhuNormal && hari < config.batas_hari_deman)
+            Console.WriteLine(config.pesan_diterima);
+        else
+            Console.WriteLine(config.pesan_ditolak);
+    }
+}
